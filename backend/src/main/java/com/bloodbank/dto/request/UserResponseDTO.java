1ae21d911dc28277fa9d0352242c029b03d05f13
Saiful Aziz <gamestopik712@gmail.com>
2026-07-09 16:52:03 +0500

    feat: add user response dto for secure response

--- backend/src/main/java/com/bloodbank/dto/request/UserResponseDTO.java
+++ backend/src/main/java/com/bloodbank/dto/request/UserResponseDTO.java
@@ -1,4 +1,24 @@
 package com.bloodbank.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserResponseDTO {
+
+    private Long userId;
+    private String email;
+    private String password;
+    private String fullName;
+    private String phone;
+    private String userRole;
+    private Boolean isActive;
+    private LocalDateTime createdAt;
 }
