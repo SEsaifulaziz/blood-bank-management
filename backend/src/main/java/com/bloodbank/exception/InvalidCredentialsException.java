54b1f9cc1ed95ac425c919a90f5b4f539eee1410
Saiful Aziz <gamestopik712@gmail.com>
2026-07-12 01:16:14 +0500

    feat: implement InvalidCredentialException for clean 401 authentication fault interception

---
+++ backend/src/main/java/com/bloodbank/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.bloodbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(final String message) {
+        super(message);
+    }
+}
