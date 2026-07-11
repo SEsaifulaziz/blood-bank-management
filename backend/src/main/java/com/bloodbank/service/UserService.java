8019c6b80525e9c4bc78a03756cb729e4218909f
Saiful Aziz <gamestopik712@gmail.com>
2026-07-11 13:21:33 +0500

    Refactor: align UserService contract to return profile DTOs

--- backend/src/main/java/com/bloodbank/service/UserService.java
+++ backend/src/main/java/com/bloodbank/service/UserService.java
@@ -3,10 +3,14 @@ package com.bloodbank.service;
 import com.bloodbank.dto.request.LoginRequestDTO;
 import com.bloodbank.dto.request.SignupRequestDTO;
 import com.bloodbank.dto.response.JWTResponseDTO;
+import com.bloodbank.dto.response.UserResponseDTO;
 
 public interface UserService {
 
-    JWTResponseDTO registerUser(SignupRequestDTO registerRequestDTO);
+//    JWTResponseDTO registerUser(SignupRequestDTO registerRequestDTO);
+//
+//    JWTResponseDTO loginUser(LoginRequestDTO loginRequestDTO);
 
-    JWTResponseDTO loginUser(LoginRequestDTO loginRequestDTO);
+    UserResponseDTO registerUSer(SignupRequestDTO dto);
+
 }
