e292d1ae34323b648a9d57d0fc50d2aaf9393caf
Saiful Aziz <gamestopik712@gmail.com>
2026-07-12 00:02:52 +0500

    fix: adjust MethodArgumentNotValidException import to web context to properly intercept DTO errors

--- backend/src/main/java/com/bloodbank/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/bloodbank/exception/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.bloodbank.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
