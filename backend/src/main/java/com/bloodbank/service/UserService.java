373bf1cf977a251013dfd42fcf077631dbfe703c
Saiful Aziz <gamestopik712@gmail.com>
2026-07-25 12:08:57 +0500

    feat: add UsernameNotFoundException to loginUser

--- backend/src/main/java/com/bloodbank/service/UserService.java
+++ backend/src/main/java/com/bloodbank/service/UserService.java
@@ -3,14 +3,12 @@ package com.bloodbank.service;
 import com.bloodbank.dto.request.LoginRequestDTO;
 import com.bloodbank.dto.request.SignupRequestDTO;
 import com.bloodbank.dto.response.JWTResponseDTO;
-import com.bloodbank.dto.response.UserResponseDTO;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import jakarta.validation.Valid;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
 
     JWTResponseDTO registerUser(SignupRequestDTO registerRequestDTO);
 
-    JWTResponseDTO loginUser(LoginRequestDTO loginRequestDTO);
+    JWTResponseDTO loginUser(LoginRequestDTO loginRequestDTO) throws UsernameNotFoundException;
 
 }
