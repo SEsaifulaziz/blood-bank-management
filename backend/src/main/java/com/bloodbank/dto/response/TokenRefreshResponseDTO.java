91a9acf3d0aa861053c75e99b8f21b0e1d1ba30a
Saiful Aziz <gamestopik712@gmail.com>
2026-07-28 23:29:28 +0500

    feat: add refresh token response DTO

--- backend/src/main/java/com/bloodbank/dto/response/TokenRefreshResponseDTO.java
+++ backend/src/main/java/com/bloodbank/dto/response/TokenRefreshResponseDTO.java
@@ -1,4 +1,20 @@
 package com.bloodbank.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class TokenRefreshResponseDTO {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+
+    public TokenRefreshResponseDTO(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
