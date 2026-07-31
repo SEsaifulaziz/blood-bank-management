9115095c18b7288881ad8cf68aa01c08c1c23340
Saiful Aziz <gamestopik712@gmail.com>
2026-07-31 23:09:41 +0500

    refactor: update imports

--- backend/src/main/java/com/bloodbank/security/config/SecurityConfig.java
+++ backend/src/main/java/com/bloodbank/security/config/SecurityConfig.java
@@ -1,15 +1,13 @@
-package com.bloodbank.config;
+package com.bloodbank.security.config;
 
+import com.bloodbank.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
