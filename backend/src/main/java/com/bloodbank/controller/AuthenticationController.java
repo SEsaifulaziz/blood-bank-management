12c7a9ab7612d1051744c7eae8892604f07a6fa7
Saiful Aziz <gamestopik712@gmail.com>
2026-08-01 15:08:45 +0500

    refactor: rename class name to AuthenticationController

--- backend/src/main/java/com/bloodbank/controller/AuthenticationController.java
+++ backend/src/main/java/com/bloodbank/controller/AuthenticationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthenticationController {
 
     private final AuthenticationService userService;
 
