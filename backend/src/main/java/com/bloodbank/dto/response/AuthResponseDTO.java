a1d39faa528e6f1fe7afa56a6790cadb62a7b1c6
Saiful Aziz <gamestopik712@gmail.com>
2026-08-02 12:59:13 +0500

    refactor(class name): update class name

--- backend/src/main/java/com/bloodbank/dto/response/AuthResponseDTO.java
+++ backend/src/main/java/com/bloodbank/dto/response/AuthResponseDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class JWTResponseDTO {
+public class AuthResponseDTO {
 
     String token;
     private String refreshToken;
