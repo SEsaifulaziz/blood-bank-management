8a7e59fcd3ad514423156fc9102f8eded17d62e1
Saiful Aziz <gamestopik712@gmail.com>
2026-08-02 13:21:03 +0500

    refactor: remove unused import

--- backend/src/main/java/com/bloodbank/service/AuthenticationService.java
+++ backend/src/main/java/com/bloodbank/service/AuthenticationService.java
@@ -3,7 +3,6 @@ package com.bloodbank.service;
 import com.bloodbank.dto.request.LoginRequestDTO;
 import com.bloodbank.dto.request.SignupRequestDTO;
 import com.bloodbank.dto.response.AuthResponseDTO;
-import com.bloodbank.dto.response.JWTResponseDTO;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface AuthenticationService {
