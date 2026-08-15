fdbddfeabed4d64741113531af5bb0f4fbe2fd87
Saiful Aziz <gamestopik712@gmail.com>
2026-08-15 12:53:58 +0500

    refactor(user): simplify User entity remove unsued imports

--- backend/src/main/java/com/bloodbank/entity/User.java
+++ backend/src/main/java/com/bloodbank/entity/User.java
@@ -2,8 +2,6 @@ package com.bloodbank.entity;
 
 import com.bloodbank.model.UserRole;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
